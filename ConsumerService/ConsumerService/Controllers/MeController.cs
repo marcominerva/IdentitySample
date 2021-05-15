f07de82bcb9407963099e50684c58fcc4ea431f7
Marco Minerva <marco.minerva@gmail.com>
2021-05-15 11:48:38 +0200

    Add consumer service project

---
+++ ConsumerService/ConsumerService/Controllers/MeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumerService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MeController : ControllerBase
+    {
+        public MeController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return NoContent();
+        }
+    }
+}
